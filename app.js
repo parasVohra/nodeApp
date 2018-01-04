c0b9354427adc6f490142ee2cd7bf6cb3814e688
parasVohra <pursharth01@gmail.com>
2018-01-04 00:25:07 -0500

    3/1/18

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get( '/', (req, res) => {
+    res.sendFile('index.html', {root:path.join(__dirname, './views')});
+});
+
+app.post('/send', (req, res) => {
+    const output = `
+        <p>You have new info </p>
+        <h3>Name : ${req.body.name} </h3>
+    `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'sg2plcpnl0102.prod.sin2.secureserver.net',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: 'test@parasvohra.com', // generated ethereal user
+            pass: 'Parasvohra@966'  // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Node mailer" <test@parasvohra.com>', // sender address
+        to: 'pursharth01@gmail.com, arogundadekolade@yahoo.com ', // list of receivers
+        subject: 'Hello ', // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+    console.log(req.body.name);
+})
+
+app.listen(3000, () => console.log("Server Started"));
+
---
+++ public/js/index.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $('.input-group input').val("");
+    $(".menuBtn").click(function(){
+        $(".secondBar").toggleClass("pRotate");
+        $(".thirdBar").toggleClass("nRotate");
+        $(".firstBar").toggleClass("hide");
+        $(".fourthBar").toggleClass("hide");
+        $('.menuScreen').toggleClass('displayBlock');
+        $('body').toggleClass('stopScroll');
+
+    })
+
+    $('.input-group input').focus(function(){
+        $(this).siblings().addClass('focused');
+    })
+
+    $('.input-group input').focusout(function(){
+        var text_val = $(this).val();
+        if(text_val === ""){
+            $(this).siblings().removeClass('focused');
+        }
+        else{
+            $(this).siblings().addClass('focused');
+        }
+        
+    })
+
+    
+});
+
+
+
+
+
+
+$(window).scroll( function() {
+
+    
+        var wScroll = $(this).scrollTop();
+        //console.log(wScroll);
+    
+    
+        // $('.topContainer').css({
+        //     'transform': 'translate(0px , -'+ wScroll / 22 +'% )'
+        // });
+
+        if (screen.width > 640){
+            $('.topBottom').css({
+                'transform': 'translate(0px , -'+ wScroll / 30 +'% )'
+            });
+        
+            $('.midLeft').css({
+                'transform': 'translate(0px , -'+ wScroll / 40 +'% )'
+            });
+        
+            $('.midBottom').css({
+                'transform': 'translate(0px , -'+ wScroll / 40 +'% )'
+            });  
+        
+            $('.bottomRight').css({
+                'transform': 'translate(0px , -'+ wScroll / 40 +'% )'
+            });
+        
+            $('.bottomBottom').css({
+                'transform': 'translate(0px , -'+ wScroll / 40 +'% )'
+            });
+        }
+    
+    
+});
